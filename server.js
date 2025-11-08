2c9646d199f3c6d0f919863cdcd7d1865bed5d91
lagerdigal1996 <kpogohhymes@hotmail.com>
2025-11-08 07:05:03 -0600

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+app.use(express.json());
+
+let connections = [];
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    connections.push(socket);
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        connections = connections.filter(conn => conn !== socket);
+    });
+
+    socket.on('update_wireframe', (data) => {
+        socket.broadcast.emit('wireframe_updated', data);
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
